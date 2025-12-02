5c9b63a555cfb93c4130d240b23c03611a275583
Baekjongwon <whddnjschlrh@naver.com>
2025-12-02 14:14:56 +0900

    assingmnent community 생성자 get setter 업데이트

--- domain/community/Announcement.java
+++ domain/community/Announcement.java
@@ -11,6 +11,41 @@ public class Announcement {
 
     public Announcement() {}
 
+    public Announcement(Long id, String title, String content, LocalDateTime createdAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
     public void create(String title, String content) {
         this.title = title;
         this.content = content;
--- domain/community/CommunityPost.java
+++ domain/community/CommunityPost.java
@@ -12,6 +12,42 @@ public class CommunityPost {
 
     public CommunityPost() {}
 
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public CommunityPost(Long id, Long authorId, String title, String content, LocalDateTime createdAt) {
+        this.id = id;
+        this.authorId = authorId;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
+
     public void create(Long authorId, String title, String content) {
         this.authorId = authorId;
         this.title = title;
