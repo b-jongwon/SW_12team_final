9eb6e065ceceea59d51851042925cd36ee22da06
ssb <tomorrowmgm1314@gmail.com>
2025-12-06 01:10:25 +0900

    기준값 설정 클래스 추가

---
+++ domain/patient/RiskConfiguration.java
@@ -0,0 +1,10 @@
+package domain.patient;
+
+public class RiskConfiguration {
+    // 기본값 설정 (관리자가 변경 가능)
+    public static double BP_SYSTOLIC_THRESHOLD = 140.0;
+    public static double BP_DIASTOLIC_THRESHOLD = 90.0;
+    public static double SUGAR_THRESHOLD = 126.0;
+    public static double BMI_THRESHOLD = 25.0;
+    public static double CHOLESTEROL_THRESHOLD = 240.0; // 신규 항목
+}
