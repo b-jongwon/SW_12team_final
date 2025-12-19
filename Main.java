8029ae44f73d1ba0e6f198e8e52a336422986acd
BaekJongwon <whddnjschlrh@naver.com>
2025-12-19 20:26:30 +0900

    원격 백업

--- Main.java
+++ Main.java
@@ -13,6 +13,7 @@ public class Main {
 
     public static void main(String[] args) {
         domain.patient.RiskConfiguration.load();
+        //
 
         clearAllData();
         System.out.println("===== Stroke Prevention System Test Start =====");
